ef5f795b7458eed4108a872d487064199d9bd028
Jacob <weaver.jacob.t@gmail.com>
2023-07-10 20:53:47 -0500

    param lab

---
+++ index.js
@@ -0,0 +1,17 @@
+const name = "Aki";
+const language = "JavaScript";
+
+function introduction(name) {
+    return "Hi, my name is" + " " + name  +".";
+}
+
+
+function introductionWithLanguage(name, language) {
+    return "Hi, my name is " + name + ' and I am learning to program in ' + language + '.';
+
+}
+
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript") {
+    return "Hi, my name is " + name + ' and I am learning to program in ' + language + '.';
+
+}
